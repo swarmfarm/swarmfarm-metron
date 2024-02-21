e00cbbbb58f529fb27700545ec168f48fd565e58
Arie <hendriksearie@gmail.com>
2024-02-22 10:22:46 +1100

    Changed name to SwarmfarmMetron

--- Package.swift
+++ Package.swift
@@ -4,20 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwamFarmMetron",
+    name: "SwarmfarmMetron",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "SwamFarmMetron",
-            targets: ["SwamFarmMetron"]),
+            name: "SwarmfarmMetron",
+            targets: ["SwarmfarmMetron"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwamFarmMetron"),
+            name: "SwarmfarmMetron"),
         .testTarget(
-            name: "SwamFarmMetronTests",
-            dependencies: ["SwamFarmMetron"]),
+            name: "SwarmfarmMetronTests",
+            dependencies: ["SwarmfarmMetron"]),
     ]
 )
